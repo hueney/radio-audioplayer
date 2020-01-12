5678376d6626a352a74e881aa490e13a02b5c5c9
Fabian Tack <fabian@kustboys.be>
2020-01-12 20:52:27 +0100

    Added settings.js file for station specific information

---
+++ dist/settings.js
@@ -0,0 +1,8 @@
+// SETTINGS FILE
+// Here you can setup your audio player with your radio station information
+var station = {
+    name: "Your station name",
+    logo: "URL to the logo",
+    stream: "URL to the stream",
+    playlist: "URL to the playlist JSON file"
+}
